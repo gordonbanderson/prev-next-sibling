2b618f3a27153f58d34ff5666f7ff5b7ebbbaf9d
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-21 01:01:16 +0700

    ENHANCEMENT: Passing tests

--- tests/PrevNextSiblingExtensionTest.php
+++ tests/PrevNextSiblingExtensionTest.php
@@ -1,12 +1,25 @@
 <?php
 
 class PrevNextSiblingExtensionTest extends SapphireTest {
+
+    protected static $fixture_file = 'PrevNextSiblingExtensionTest.yml';
+
 	public function testNextSibling() {
-		$this->markTestSkipped('TODO');
+		$child1 = $this->objFromFixture('Page', 'child01');
+        $child2 = $this->objFromFixture('Page', 'child02');
+        $child3 = $this->objFromFixture('Page', 'child03');
+        $this->assertNull($child3->NextSibling());
+        $this->assertEquals($child2, $child1->NextSibling());
+        $this->assertEquals($child3, $child2->NextSibling());
 	}
 
 	public function testPreviousSibling() {
-		$this->markTestSkipped('TODO');
+		$child1 = $this->objFromFixture('Page', 'child01');
+        $this->assertNull($child1->PreviousSibling());
+        $child2 = $this->objFromFixture('Page', 'child02');
+        $this->assertEquals($child1, $child2->PreviousSibling());
+        $child3 = $this->objFromFixture('Page', 'child03');
+        $this->assertEquals($child2, $child3->PreviousSibling());
 	}
 
 }

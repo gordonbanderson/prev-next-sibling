6a3d29ad07b818e3aa2f0de53d83cde04dda4f42
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-20 23:45:12 +0700

    WIP: Initial tests shell

---
+++ tests/PrevNextSiblingExtensionTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class PrevNextSiblingExtensionTest extends SapphireTest {
+	public function testNextSibling() {
+		$this->markTestSkipped('TODO');
+	}
+
+	public function testPreviousSibling() {
+		$this->markTestSkipped('TODO');
+	}
+
+}

af7aaf494e971521f531322071ae5641d28cc131
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-02-06 12:03:16 +0700

    FIX: Quoted column names for Postgres happiness

--- code/PrevNextSiblingExtension.php
+++ code/PrevNextSiblingExtension.php
@@ -6,14 +6,14 @@ class PrevNextSiblingExtension extends DataExtension
 {
     public function NextSibling()
     {
-        $where = "ParentID = {$this->owner->ParentID} AND Sort > {$this->owner->Sort}";
-        $result = SiteTree::get()->where($where)->sort('Sort')->First();
+        $where = "\"ParentID\" = {$this->owner->ParentID} AND \"Sort\" > {$this->owner->Sort}";
+        $result = SiteTree::get()->where($where)->sort('"Sort"')->First();
         return $result;
     }
 
     public function PreviousSibling()
     {
-        $where = "ParentID = {$this->owner->ParentID} AND Sort < {$this->owner->Sort}";
-        return SiteTree::get()->where($where)->sort('Sort DESC')->First();
+        $where = "\"ParentID\" = {$this->owner->ParentID} AND \"Sort\" < {$this->owner->Sort}";
+        return SiteTree::get()->where($where)->sort('"Sort" DESC')->First();
     }
 }

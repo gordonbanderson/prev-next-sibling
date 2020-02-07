38adf67c009eda0bff9704a4380e5f7b06efcc1d
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-02-07 23:50:22 +0000

    FIX: Use IDs instead of comparing objects directly

--- tests/PrevNextSiblingExtensionTest.php
+++ tests/PrevNextSiblingExtensionTest.php
@@ -1,25 +1,28 @@
 <?php
+namespace WebOfTalent\PrevNextSibling\Tests;
+
+use SilverStripe\Dev\SapphireTest;
 
 class PrevNextSiblingExtensionTest extends SapphireTest {
 
-    protected static $fixture_file = 'PrevNextSiblingExtensionTest.yml';
+    protected static $fixture_file = 'tests/PrevNextSiblingExtensionTest.yml';
 
 	public function testNextSibling() {
 		$child1 = $this->objFromFixture('Page', 'child01');
         $child2 = $this->objFromFixture('Page', 'child02');
         $child3 = $this->objFromFixture('Page', 'child03');
         $this->assertNull($child3->NextSibling());
-        $this->assertEquals($child2, $child1->NextSibling());
-        $this->assertEquals($child3, $child2->NextSibling());
+        $this->assertEquals($child2->ID, $child1->NextSibling()->ID);
+        $this->assertEquals($child3->ID, $child2->NextSibling()->ID);
 	}
 
 	public function testPreviousSibling() {
 		$child1 = $this->objFromFixture('Page', 'child01');
         $this->assertNull($child1->PreviousSibling());
         $child2 = $this->objFromFixture('Page', 'child02');
-        $this->assertEquals($child1, $child2->PreviousSibling());
+        $this->assertEquals($child1->ID, $child2->PreviousSibling()->ID);
         $child3 = $this->objFromFixture('Page', 'child03');
-        $this->assertEquals($child2, $child3->PreviousSibling());
+        $this->assertEquals($child2->ID, $child3->PreviousSibling()->ID);
 	}
 
 }

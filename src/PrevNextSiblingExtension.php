80a12941339b65b1a0947bec7bb3655ae2dad88a
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-06-10 22:39:08 +0100

    WIP: checkcs now passes

--- src/PrevNextSiblingExtension.php
+++ src/PrevNextSiblingExtension.php
@@ -5,17 +5,23 @@ namespace WebOfTalent\PrevNextSibling;
 use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Core\Extension;
 
+/**
+ * Class PrevNextSiblingExtension
+ *
+ * @package WebOfTalent\PrevNextSibling
+ * @phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+ */
 class PrevNextSiblingExtension extends Extension
 {
-    public function NextSibling()
+    public function NextSibling(): ?\SilverStripe\ORM\DataObject
     {
         $where = "\"ParentID\" = {$this->owner->ParentID} AND \"Sort\" > {$this->owner->Sort}";
 
         return SiteTree::get()->where($where)->sort('"Sort"')->First();
     }
 
-
-    public function PreviousSibling()
+    
+    public function PreviousSibling(): ?\SilverStripe\ORM\DataObject
     {
         $where = "\"ParentID\" = {$this->owner->ParentID} AND \"Sort\" < {$this->owner->Sort}";
 
